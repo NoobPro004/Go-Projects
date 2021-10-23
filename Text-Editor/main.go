e83d82fc5c886e6e77c0b1074ae2cedb1e281d4a
NoobPro004 <himanshugarg738@gmail.com>
2021-10-24 02:00:56 +0530

    added upto Ui of Text-Editor

---
+++ Text-Editor/main.go
@@ -0,0 +1,52 @@
+package main
+
+import (
+	"strconv"
+
+	"fyne.io/fyne/v2"
+	"fyne.io/fyne/v2/app"
+	"fyne.io/fyne/v2/container"
+	"fyne.io/fyne/v2/widget"
+)
+
+var count int =1;
+
+func main() {
+	a := app.New()
+	w := a.NewWindow("Text-Editor")
+	w.Resize(fyne.NewSize(800,800));
+	
+	content:= container.NewVBox(
+		container.NewHBox(
+			widget.NewLabel("Text Editor"),
+		),
+	)
+
+	content.Add(widget.NewButton("Add New File",func() {
+		content.Add(widget.NewLabel("New File"+strconv.Itoa(count)));
+		count++;
+	}))
+
+	input := widget.NewMultiLineEntry();
+	input.SetPlaceHolder("Enter text .....");
+	// input.Resize(fyne.NewSize(600,100));
+
+	saveBtn := widget.NewButton("Save text File",func() {
+		
+	})
+
+
+
+	w.SetContent(
+		container.NewVBox(
+			content,
+			input,
+			container.NewHBox(
+				saveBtn,
+			),
+		),
+
+		
+	)
+	w.ShowAndRun()
+}
